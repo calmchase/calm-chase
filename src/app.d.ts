f1fc80b76f6364c4c613033696a3c0e8d2f05e51
Raj <rajesh-pandey@outlook.in>
2024-10-15 17:36:38 +0530

    feat: added cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -26,7 +26,15 @@ declare global {
     // interface Locals {}
     // interface PageData {}
     // interface PageState {}
-    // interface Platform {}
+    interface Platform {
+      env: {
+        COUNTER: DurableObjectNamespace;
+      };
+      context: {
+        waitUntil(promise: Promise<unknown>): void;
+      };
+      caches: CacheStorage & { default: Cache };
+    }
   }
 }
 
