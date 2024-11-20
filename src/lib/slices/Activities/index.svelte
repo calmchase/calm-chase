29e85d7062ba1d49752d93a02f6c872973105003
Raj <rajesh-pandey@outlook.in>
2024-11-20 18:25:46 +0530

    fix: added buttons

--- src/lib/slices/Activities/index.svelte
+++ src/lib/slices/Activities/index.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import * as Drawer from "$lib/components/ui/drawer/index.js";
+  import ShadButton from "@/components/ui/button/button.svelte";
+  import { cn } from "@/utils";
+
   import type { Content } from "@prismicio/client";
   import { PrismicImage, PrismicRichText } from "@prismicio/svelte";
   import { Button } from "bits-ui";
@@ -8,6 +11,7 @@
 
   export let slice: Content.ActivitiesSlice;
   let open = new Array(slice.primary.activities.length).fill(false);
+  let read = false;
   const isDesktop = mediaQuery("(min-width: 768px)");
 </script>
 
@@ -37,7 +41,14 @@
             </Dialog.Trigger>
             <Dialog.Content
               class="flex max-w-6xl items-center gap-10 bg-yellow1 p-10">
-              <PrismicImage field="{element.square}" class="h-72 w-72" />
+              <div class="w-full">
+                <PrismicImage field="{element.square}" class="h-72 w-72" />
+                <ShadButton
+                  type="submit"
+                  class="mt-4 w-full self-end rounded-full bg-blue1 p-5 font-int text-base font-semibold text-white1 hover:bg-black1   active:bg-black1  md:w-72">
+                  Join community
+                </ShadButton>
+              </div>
               <div class="text-black">
                 <Dialog.Title class="pb-6 font-int text-4.5xl font-semibold">
                   {element.title}
@@ -50,6 +61,7 @@
           <Drawer.Root
             shouldScaleBackground
             snapPoints="{[0.8]}"
+            onClose="{() => (read = false)}"
             bind:open="{open[i]}">
             <Drawer.Trigger asChild let:builder>
               <Button.Root
@@ -62,7 +74,7 @@
                 </h6>
               </Button.Root>
             </Drawer.Trigger>
-            <Drawer.Content class="h-full bg-yellow1 px-5">
+            <Drawer.Content class="h-full bg-yellow1 px-5 pb-20">
               <div class="flex flex-col overflow-y-auto pb-38 pt-4">
                 <Drawer.Header class="h-auto w-full">
                   <PrismicImage field="{element.wide}" />
@@ -70,7 +82,23 @@
                 <Drawer.Title class="pb-6 font-int text-xl font-semibold">
                   {element.title}
                 </Drawer.Title>
-                <PrismicRichText field="{element.content}" />
+                <p class="{cn({ 'line-clamp-5': !read })}">
+                  <PrismicRichText field="{element.content}" />
+                </p>
+
+                <ShadButton variant="link" on:click="{() => (read = !read)}">
+                  {#if read}
+                    Show less
+                  {:else}
+                    Read more
+                  {/if}
+                </ShadButton>
+
+                <ShadButton
+                  type="submit"
+                  class="mt-4 w-full self-end rounded-full bg-blue1 p-5 font-int text-base font-semibold text-white1 hover:bg-yellow1 hover:text-blue1 active:bg-yellow1 active:text-blue1 md:w-fit">
+                  Join community
+                </ShadButton>
               </div>
             </Drawer.Content>
           </Drawer.Root>
