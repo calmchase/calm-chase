7ecd7695d1c4ec7635a33381d357a5e399830628
Raj <rajesh-pandey@outlook.in>
2024-11-25 14:40:57 +0530

    fix: removed problem statement

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
   import { components } from "$lib/slices";
-  import { Button } from "@/components/ui/button";
   import { SliceZone } from "@prismicio/svelte";
-  import Download from "~icons/mingcute/file-download-fill";
   export let data;
 </script>
 
 <SliceZone slices="{data.page.data.slices}" {components} />
-
-<Button
-  on:click="{async () => {
-    const link = document.createElement('a');
-    link.download = 'Problem.pdf';
-    link.rel = 'noopener';
-    link.href = '/problems.pdf';
-    link.click();
-    // Revoke the object URL to free up memory
-    URL.revokeObjectURL(link.href);
-  }}"
-  variant="outline"
-  class="fixed bottom-5 right-5 h-12 animate-bounce bg-blue1 p-2">
-  <Download class="h-10 w-10 text-yellow1" />
-</Button>
