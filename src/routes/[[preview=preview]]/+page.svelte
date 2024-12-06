0efd076f640315469dc7b286adce676f3fb9e561
Raj <rajesh-pandey@outlook.in>
2024-12-06 22:21:49 +0530

    chore: added results

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -4,4 +4,12 @@
   export let data;
 </script>
 
+<p class=" w-full bg-yellow1 text-center text-black1">
+  Hackathon results are out!!. <a
+    target="_blank"
+    href="http://calmchase.com/results.pdf"
+    class="animate-pulse font-bold underline">
+    Click here to See your score now !
+  </a>
+</p>
 <SliceZone slices="{data.page.data.slices}" {components} />
