d24c7d77a436454c28c5f3b1970e76adef6472dd
Raj <rajesh-pandey@outlook.in>
2024-12-15 11:09:47 +0530

    fix: added height adjustment and links

--- src/lib/slices/Programs/index.svelte
+++ src/lib/slices/Programs/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import * as Card from "$lib/components/ui/card/index.js";
   import type { Content } from "@prismicio/client";
+  import { PrismicLink } from "@prismicio/svelte";
 
   export let slice: Content.ProgramsSlice;
 </script>
@@ -13,10 +14,11 @@
     class="col-span-full mb-4 mt-6 font-grot text-2xl font-black text-grey1 md:row-span-full md:mb-8 md:mt-13 md:text-5xl">
     OUR PROGRAMS
   </p>
-  <div class="grid grid-cols-2 gap-3 px-2 md:grid-cols-4 md:gap-6 md:px-6">
+  <div
+    class="grid grid-cols-2 items-stretch gap-3 px-2 md:grid-cols-4 md:gap-6 md:px-6">
     {#each slice.primary.programs as element}
-      <a href="/contact">
-        <Card.Root class=" bg-yellow1  ">
+      <PrismicLink field="{element.goto}" class="h-full">
+        <Card.Root class=" h-full bg-yellow1 ">
           <Card.Content class="flex flex-col gap-2 px-1 py-8 md:px-6">
             <Card.Title
               class="text-base font-semibold text-grey1 md:text-3xl  ">
@@ -28,7 +30,7 @@
             </Card.Description>
           </Card.Content>
         </Card.Root>
-      </a>
+      </PrismicLink>
     {/each}
   </div>
 </section>
